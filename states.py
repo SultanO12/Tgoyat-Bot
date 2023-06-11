aeaa5f0b819d58fff54b0db477c6819f18913907
SultanO12 <sultanotabekov111@gmail.com>
2023-06-11 12:04:56 +0300

    Update

---
+++ states.py
@@ -0,0 +1,8 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+class SuralarInfo(StatesGroup):
+    sura = State()
+    oyat = State()
+
+class SearchSura(StatesGroup):
+    sura_name = State()
